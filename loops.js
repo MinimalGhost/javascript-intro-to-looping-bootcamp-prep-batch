065a17c6174011265182f09369fcea771ab49426
Eric Kollegger <eric.kollegger@gmail.com>
2017-10-21 19:54:07 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,20 +1,32 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
     if (i === 1) {
-      array.push(`I am ${i} strange loop.`);
+      array.push(`I am ${i} strange loop.`)
     } else {
-      array.push(`I am ${i} strange loops.`);
+      array.push(`I am ${i} strange loops.`)
     }
   }
   return array
 }
 
 function whileLoop(n) {
-  while (n > 0) {
-    if (n > 0) {
+  while (n >= 0) {
+    n--
+    if (n >= 0) {
       console.log(n);
     } else {
-      return n
+      return 'done'
     }
   }
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.splice(-1, 1)
+  } while (array.length > 0 || maybeTrue());
+  return array
 }
